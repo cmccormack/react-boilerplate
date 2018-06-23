8092f04fdddae3f88adaf49298c76c04dde1c5a4
Christopher McCormack <mccormack.christopher@gmail.com>
2018-06-23 12:33:09 -0700

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import "./assets/images/favicon.ico"
+import "./assets/styles/styles.scss"
+import App from "./views/App"
+
+const root = document.getElementById("root")
+ReactDOM.render(
+  <App />,
+  root,
+)
